36eb38971970cb2ef87f2c9ec9876b7f555f8cfe
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2025-01-22 13:32:40 +0600

    added data

---
+++ Main_Part/Data/Migrations/20250121215412_AddCancelDateTicketGen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Main_Part.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCancelDateTicketGen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CancelDeadline",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TicketNumber",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelDeadline",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "TicketNumber",
+                table: "Bookings");
+        }
+    }
+}
