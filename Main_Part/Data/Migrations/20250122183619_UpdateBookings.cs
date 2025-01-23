1709ba57aec127fd7952752857bd83dd52e20577
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2025-01-23 22:36:10 +0600

    update design

---
+++ Main_Part/Data/Migrations/20250122183619_UpdateBookings.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Main_Part.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BookUserNsme",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentOption",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookUserNsme",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentOption",
+                table: "Bookings");
+        }
+    }
+}
